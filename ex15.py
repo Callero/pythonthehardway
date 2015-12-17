69b4254fc62da09b8f66a5aa897967ef17bd9eb3
Calle Liljeholm <carladam.liljeholm@emc.com>
2015-12-17 22:22:39 +0100

    .

--- ex15.py
+++ ex15.py
@@ -1,16 +1,16 @@
-# Imports module
-from sys import argv
-
-#arguments for command line
-script, filename = argv
-
-#a variable called 'txt' that contains the filename from the command line
-txt = open(filename)
-
-#prints the file name from argv
-print "Here's your file %r:" % filename
-#reads and prints the 'txt' variable
-print txt.read()
+# # Imports module
+# from sys import argv
+#
+# #arguments for command line
+# script, filename = argv
+#
+# #a variable called 'txt' that contains the filename from the command line
+# txt = open(filename)
+#
+# #prints the file name from argv
+# print "Here's your file %r:" % filename
+# #reads and prints the 'txt' variable
+# print txt.read()
 
 print "Type the filename again:"
 #fetching the file name
@@ -20,4 +20,8 @@ file_again = raw_input("> ")
 txt_again = open(file_again)
 
 #prints the file contents
-print txt_again.read()
+print txt_again.readline()
+
+txt_again.close()
+
+
