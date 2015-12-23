a13fe92f288773a4ff508b23de1acbbd4cbcc888
Calle Liljeholm <carladam.liljeholm@emc.com>
2015-12-23 14:45:31 +0100

    Answered ex31 study drills

--- ex31.py
+++ ex31.py
@@ -6,6 +6,7 @@ if door == "1":
     print "There's a giant bear here eating a cheese cake. What do you do?"
     print "1. Take the cake."
     print "2. Scream at the bear."
+    print "3. Hug the bear."
 
     bear = raw_input("> ")
 
@@ -13,6 +14,8 @@ if door == "1":
         print "The bear eats your face off. Good job!"
     elif bear == "2":
         print "The bear eats your leg off. Good job!"
+    elif bear == "3":
+        print "The bear squishes you to goo. Good job!"
     else:
         print "Well, doing %s is probably better. Bear runs away." % bear
 
@@ -30,4 +33,6 @@ elif door == "2":
         print "The insanity rots your eyes into a pool of muck. Good job!"
 
 else:
-    print "You stumble around and fall on a knife and die. Good job!"
+    print "Gooble?"
+    gooble = raw_input("> ")
+    print "Yes, with %s gooble, you stumble around and fall on a knife and die. Good job!" % gooble
