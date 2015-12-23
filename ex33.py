9e6e33238afa100c8c36fd1d0f86f2015997839b
Calle Liljeholm <carladam.liljeholm@emc.com>
2015-12-23 17:22:58 +0100

    ex33 study drill 1

--- ex33.py
+++ ex33.py
@@ -1,15 +1,30 @@
-i = 0
-numbers = []
+# while i < 6:
+#     print "At the top i is %d" % i
+#     numbers.append(i)
+#
+#     i = i + 1
+#     print "Numbers now: ", numbers
+#     print "At the bottom i is % d" % i
+#
+# print "The numbers: "
+#
+# for num in numbers:
+#     print(num)
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def printstuff(iterations):
+    i = 0
+    numbers = []
+    while i < iterations:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is % d" % i
+        i += 1
 
-print "The numbers: "
+        print "Numbers now: ", numbers
+        print "At the bottom i is % d" % i
 
-for num in numbers:
-    print(num)
+    for num in numbers:
+        print(num)
+
+
+printstuff(4)
