a402447629a7ea84576e954139b857f9307e652b
Calle Liljeholm <carladam.liljeholm@emc.com>
2015-12-27 21:01:05 +0100

    done with excersises in ex37.

---
+++ ex37.py
@@ -0,0 +1,30 @@
+# print "Try 'del'"
+# a = ["first", "second"]
+# print "before del"
+# print a
+# print "del a[0]"
+# del a[0]
+# print a
+
+# print "try 'while'"
+# print "while a < 5 print value"
+# a = 0
+# while a < 5:
+#     print a
+#     a += 1
+
+# import unittest
+#
+# print "try 'assert'"
+#
+# def KelvinToFahrenheit(Temperature):
+#    assert (Temperature >= 0),"Colder than absolute zero!"
+#    return ((Temperature-273)*1.8)+32
+#
+# print KelvinToFahrenheit(273)
+# print int(KelvinToFahrenheit(505.78))
+# print KelvinToFahrenheit(-5)
+
+# g = lambda x: x-2
+# print g(8)
+
